a87a7d26bfec3ade5e61f237f7791801c63fbde7
jeremy BERGUIG <jberguig@a205-12.iut.univ-paris8.fr>
2023-05-22 09:43:23 +0200

    Feat: chargerQuestionnaire sous format BO

--- src/main/java/fr/iut/montreuil/s4_02_2023/moyai_questionnaire/modeles/Questionnaire.java
+++ src/main/java/fr/iut/montreuil/s4_02_2023/moyai_questionnaire/modeles/Questionnaire.java
@@ -21,7 +21,32 @@ public class Questionnaire {
 //        return ;
 //    }
     public ListQuestionBO chargerQuestionnaire(String urlFicherCSV) throws FichierIntrouvable, FormatInvalide{
-        return null;
+        List<List<String>> l = new ArrayList<>();
+        File provFile = new File("");
+        try {
+            System.out.println(provFile.getAbsoluteFile()+"\\src\\main\\java\\fr\\iut\\montreuil\\s4_02_2023\\moyai_questionnaire\\resources\\"+urlFicherCSV);
+            BufferedReader br = new BufferedReader(new FileReader(provFile.getAbsoluteFile()+"\\src\\main\\java\\fr\\iut\\montreuil\\s4_02_2023\\moyai_questionnaire\\resources\\"+urlFicherCSV));
+            String ligne = null;
+            while ((ligne = br.readLine()) != null) {
+                // Retourner la ligne dans un tableau
+                String[] data = ligne.split(";");
+                if (data.length != 8) throw new FormatInvalide();
+                ArrayList<String>  stringArrayList = new ArrayList<String>();
+                // Afficher le contenu du tableau
+                for (String val : data) {
+                    stringArrayList.add(val);
+                    System.out.println(val);
+                }
+                System.out.println(stringArrayList);
+                l.add(stringArrayList);
+            }
+            br.close();
+        } catch (FileNotFoundException e) {
+            throw new FichierIntrouvable();
+        } catch (IOException e) {
+
+        }
+        return new ListQuestionBO(l);
     }
 
 
