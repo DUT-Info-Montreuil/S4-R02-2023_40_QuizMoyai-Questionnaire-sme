4cc5e47b415ed7a55e9c52a90ed9f613fadff034
Fabrice <fabrice.amegadjen@gmail.com>
2023-05-30 14:53:01 +0200

    Test: - fournirStatQuestionnaire

--- src/test/java/fr/iut/montreuil/S4_R02_2023/moyai_questionnaire/impl/QuestionnaireTest.java
+++ src/test/java/fr/iut/montreuil/S4_R02_2023/moyai_questionnaire/impl/QuestionnaireTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.io.File;
 import java.io.IOException;
 
-import fr.iut.montreuil.s4_02_2023.moyai_questionnaire.modeles.exceptions.FichierVide;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,12 +23,12 @@ import fr.iut.montreuil.s4_02_2023.moyai_questionnaire.entities.bo.ListQuestionB
 import fr.iut.montreuil.s4_02_2023.moyai_questionnaire.entities.bo.QuestionBO;
 import fr.iut.montreuil.s4_02_2023.moyai_questionnaire.entities.dto.ListQuestionDTO;
 import fr.iut.montreuil.s4_02_2023.moyai_questionnaire.entities.dto.QuestionDTO;
+import fr.iut.montreuil.s4_02_2023.moyai_questionnaire.entities.dto.StatsQuestionnaireDTO;
 import fr.iut.montreuil.s4_02_2023.moyai_questionnaire.modeles.LangueEnum;
-import fr.iut.montreuil.s4_02_2023.moyai_questionnaire.modeles.Question;
-import fr.iut.montreuil.s4_02_2023.moyai_questionnaire.modeles.Questionnaire;
-import fr.iut.montreuil.s4_02_2023.moyai_questionnaire.modeles.StatsQuestionnaire;
-import fr.iut.montreuil.s4_02_2023.moyai_questionnaire.modeles.exceptions.FichierIntrouvable;
-import fr.iut.montreuil.s4_02_2023.moyai_questionnaire.modeles.exceptions.FormatInvalide;
+import fr.iut.montreuil.s4_02_2023.moyai_questionnaire.modeles.QuestionnaireService;
+import fr.iut.montreuil.s4_02_2023.moyai_questionnaire.utils.FichierIntrouvableException;
+import fr.iut.montreuil.s4_02_2023.moyai_questionnaire.utils.FichierVideException;
+import fr.iut.montreuil.s4_02_2023.moyai_questionnaire.utils.FormatInvalideException;
 
 
 
@@ -50,12 +50,20 @@ public class QuestionnaireTest {
     		"Le joueur peut poser sa balle sur une cheville de bois ou de plastique qui ne peut pas être utilisée en dehors des départs.", "https://fr.wikipedia.org/wiki/Matériel_de_golf");
 	
 	@Mock
-	QuestionDTO Question2;
+	QuestionDTO question2;
 	
 	@Mock
-	QuestionDTO Question3;
+	QuestionDTO question3;
 	
-	private Questionnaire questionnaireService = new Questionnaire() ;
+
+	private QuestionnaireService questionnaireService = new QuestionnaireService() ;
+	
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		question2 = Mockito.mock(QuestionDTO.class);
+		question3 = Mockito.mock(QuestionDTO.class);
+	}
 
 	@Test
 	public void chargerQuestionnaireEgaliterLigneUneTest() throws CsvValidationException, IOException  {
@@ -71,10 +79,10 @@ public class QuestionnaireTest {
 		Assertions.assertEquals(listQuestionBO.getLines().get(0).getLibellé(),  Question1.getLibellé());
 		Assertions.assertEquals(listQuestionBO.getLines().get(0).getNum(),  Question1.getNum());
 		Assertions.assertEquals(listQuestionBO.getLines().get(0).getRéponse(),  Question1.getRéponse());
-	} catch (FichierIntrouvable e) {
+	} catch (FichierIntrouvableException e) {
 		System.out.println("hello FichierIntrouvable");
 		e.printStackTrace();
-	} catch (FormatInvalide e) {
+	} catch (FormatInvalideException e) {
 		System.out.println("hello FormatInvalide");
 		e.printStackTrace();
 	}
@@ -85,8 +93,8 @@ public class QuestionnaireTest {
 	@Test
 	public void chargerQuestionnaireFichierIntrouvableTest() throws CsvValidationException, IOException  {
 		cheminFichierCsvIncorrecte = getCSVFiles.getAbsolutePath() + "/src/test/java/fr/iut/montreuil/S4_R02_2023/moyai_questionnaire/resources/" + nomFichierCsvInexistant ;
-		final Questionnaire questionnaire = new Questionnaire();
-		assertThrows(FichierIntrouvable.class, new Executable() {
+		final QuestionnaireService questionnaire = new QuestionnaireService();
+		assertThrows(FichierIntrouvableException.class, new Executable() {
 			@Override
 			public void execute() throws Throwable {questionnaire.chargerQuestionnaire(cheminFichierCsvIncorrecte);}
 		});
@@ -95,8 +103,8 @@ public class QuestionnaireTest {
 		@Test
 	public void chargerQuestionnaireFichierVideTest() throws CsvValidationException, IOException  {
 		cheminFichierCsvCorrecte = getCSVFiles.getAbsolutePath() + "/src/test/java/fr/iut/montreuil/S4_R02_2023/moyai_questionnaire/resources/" + nomFichierCsv ;
-		final Questionnaire questionnaire = new Questionnaire();
-		assertThrows(FormatInvalide.class, (new Executable() {
+		final QuestionnaireService questionnaire = new QuestionnaireService();
+		assertThrows(FormatInvalideException.class, (new Executable() {
 				@Override
 				public void execute() throws Throwable {questionnaire.chargerQuestionnaire(cheminFichierCsvVide);}
 		}));
@@ -105,10 +113,10 @@ public class QuestionnaireTest {
 	}
 
 	@Test
-	public void chargerQuestionnaireFichierFormatInvalideTest() throws CsvValidationException, IOException, FormatInvalide, FichierIntrouvable {
+	public void chargerQuestionnaireFichierFormatInvalideTest() throws CsvValidationException, IOException, FormatInvalideException, FichierIntrouvableException {
 		cheminFichierCsvIncorrecte = getCSVFiles.getAbsolutePath() + "/src/test/java/fr/iut/montreuil/S4_R02_2023/moyai_questionnaire/resources/" + nomFicherFormatInvalide ;
-		final Questionnaire questionnaire = new Questionnaire();
-		assertThrows(FormatInvalide.class, (new Executable() {
+		final QuestionnaireService questionnaire = new QuestionnaireService();
+		assertThrows(FormatInvalideException.class, (new Executable() {
 			@Override
 			public void execute() throws Throwable {questionnaire.chargerQuestionnaire(cheminFichierCsvIncorrecte);}
 		}));
@@ -116,24 +124,111 @@ public class QuestionnaireTest {
 	}
 	
 	@Test
-	public void fournirStatsQuestionnaireCasNormaleTest() throws CsvValidationException, IOException  {
-		Question2.getStatsQuestion().setNbFois(1);
-		Question3.getStatsQuestion().setNbFois(2);
+	public void fournirStatsQuestionnaireNbFoisJouerTest() throws CsvValidationException, IOException  {
+		question2.getStatsQuestion().setNbFois(1);
+		question3.getStatsQuestion().setNbFois(2);
 		
-		StatsQuestionnaire statQuestionnaire = Mockito.mock(StatsQuestionnaire.class);
+		StatsQuestionnaireDTO statQuestionnaire = Mockito.mock(StatsQuestionnaireDTO.class);
 		
 		ListQuestionDTO questionList = new ListQuestionDTO(statQuestionnaire);
-		questionnaireService = new Questionnaire();
+		questionnaireService = new QuestionnaireService();
+		questionList.add(question2);
+		questionList.add(question3);
+		questionnaireService = new QuestionnaireService();
 		questionnaireService.setQuestionList(questionList);
 		
-		Assertions.assertEquals(questionnaireService.getQuestionList().getStatsQuestionnaire().getNbfois(),  3);
 		
+		Assertions.assertEquals(questionnaireService.getQuestionList().getStatsQuestionnaire().getNbfois(),  2);
 		
-	
-		
-		
+		questionList.getL().remove(question3);
+		questionnaireService.setQuestionList(questionList);
+		Assertions.assertEquals(questionnaireService.getQuestionList().getStatsQuestionnaire().getNbfois(),  1);
 		
 	}
 	
+	@Test
+	public void fournirStatsQuestionnaireMeilleureQuestionTest() throws CsvValidationException, IOException  {
+		question2.getStatsQuestion().setNbFois(20);
+		question2.getStatsQuestion().setNbReussi(10);
+		question3.getStatsQuestion().setNbFois(10);
+		question3.getStatsQuestion().setNbReussi(3);
+		
+		StatsQuestionnaireDTO statQuestionnaire = Mockito.mock(StatsQuestionnaireDTO.class);
+		ListQuestionDTO questionList = new ListQuestionDTO(statQuestionnaire);
+		questionList.add(question2);
+		questionList.add(question3);
+		questionnaireService = new QuestionnaireService();
+		questionnaireService.setQuestionList(questionList);
+		
+		Assertions.assertEquals(questionnaireService.getQuestionList().getStatsQuestionnaire().getMeilleurQuestion(),  question2);
+		
+		question3.getStatsQuestion().setNbReussi(5);
+		question2.setDifficulté(3);
+		question3.setDifficulté(4);
+		
+		Assertions.assertEquals(questionnaireService.getQuestionList().getStatsQuestionnaire().getMeilleurQuestion(),  question3);
+		
+		question2.setDifficulté(4);
+		
+		Assertions.assertEquals(questionnaireService.getQuestionList().getStatsQuestionnaire().getMeilleurQuestion(),  question2);
+		
+		question3.getStatsQuestion().setNbFois(20);
+		question3.getStatsQuestion().setNbReussi(10);
+		question2.setNum(3);
+		question3.setNum(2);
+		
+		Assertions.assertEquals(questionnaireService.getQuestionList().getStatsQuestionnaire().getMeilleurQuestion(),  question3);
+		
+	}
+	
+	
+	@Test
+	public void fournirStatsQuestionnairePireQuestionTest() throws CsvValidationException, IOException  {
+		question2.getStatsQuestion().setNbFois(20);
+		question2.getStatsQuestion().setNbReussi(2);
+		
+		question3.getStatsQuestion().setNbFois(10);
+		question3.getStatsQuestion().setNbReussi(6);
+		
+		StatsQuestionnaireDTO statQuestionnaire = Mockito.mock(StatsQuestionnaireDTO.class);
+		ListQuestionDTO questionList = new ListQuestionDTO(statQuestionnaire);
+		questionList.add(question2);
+		questionList.add(question3);
+		questionnaireService = new QuestionnaireService();
+		questionnaireService.setQuestionList(questionList);
+		
+		Assertions.assertEquals(questionnaireService.getQuestionList().getStatsQuestionnaire().getPireQuestion(),  question2);
+		
+		question3.getStatsQuestion().setNbReussi(5);
+		question2.setDifficulté(4);
+		question3.setDifficulté(3);
+		
+		Assertions.assertEquals(questionnaireService.getQuestionList().getStatsQuestionnaire().getMeilleurQuestion(),  question3);
+		
+		question3.setDifficulté(4);
+		
+		Assertions.assertEquals(questionnaireService.getQuestionList().getStatsQuestionnaire().getMeilleurQuestion(),  question2);
+		
+		question3.getStatsQuestion().setNbFois(20);
+		question3.getStatsQuestion().setNbReussi(2);
+		question2.setNum(3);
+		question3.setNum(2);
+		
+		Assertions.assertEquals(questionnaireService.getQuestionList().getStatsQuestionnaire().getMeilleurQuestion(),  question3);
+		
+	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
